4a4859b285b7f26853a4297ab6b267d4c1cccdf4
Adam Michna <adam@feelit.dev>
2025-04-11 15:27:10 +0200

    Add read data

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import FastAPI
 
 from pydantic import BaseModel
+import json
 
 
 class Request(BaseModel):
@@ -14,17 +15,39 @@ app = FastAPI()
 
 @app.post("/tool1")
 async def tool_1(request: Request):
-    result = {
-        "output": request.input,
-    }
+    if request.input.startswith("test"):
+        result = {
+            "output": request.input,
+        }
+    else:
+        with open("files/uczelnie.json", "r", encoding="utf-8") as f:
+            uczelnie = json.load(f)
+            uczelnie_dict = {u["id"]: u for u in uczelnie}
+
+        with open("files/osoby.json", "r", encoding="utf-8") as f:
+            osoby = json.load(f)
+
+            # Łączenie danych
+            result = []
+            for osoba in osoby:
+                uczelnia_info = uczelnie_dict.get(osoba["uczelnia"], {})
+                full_data = {**osoba, **{
+                    "nazwa_uczelni": uczelnia_info.get("nazwa"),
+                    "miasto_uczelni": uczelnia_info.get("miasto")
+                }}
+                result.append(full_data)
 
     return result
 
 
 @app.post("/tool2")
 async def tool_2(request: Request):
-    result = {
-        "output": request.input,
-    }
+    if request.input.startswith("test"):
+        result = {
+            "output": request.input,
+        }
+    elif request.input == "badania":
+        with open("files/badania.json", "r") as f:
+            result = json.load(f)
 
     return result
