99c211a84b23b9a036dc2502a2cf8b9fcf9783e7
Adam Michna <adam@feelit.dev>
2025-04-25 20:06:32 +0200

    Change

--- main.py
+++ main.py
@@ -30,9 +30,14 @@ async def tool_1(request: Request):
         }
         return result
     else:
+        print(f"Request: {request.input}")
         with open("files/uczelnie.json", "r", encoding="utf-8") as f:
             uczelnie = json.load(f)
             uczelnie_dict = {u["id"]: u for u in uczelnie}
+            uczelnia_info = uczelnie_dict.get(request.input, False)
+            if uczelnia_info:
+                # Jeśli uczelnia istnieje, zwróć jej dane
+                return uczelnia_info
 
         with open("files/osoby.json", "r", encoding="utf-8") as f:
             osoby = json.load(f)
@@ -43,12 +48,7 @@ async def tool_1(request: Request):
                 full_name = osoba["imie"] + " " + osoba["nazwisko"]
                 if request.input.lower() in full_name.lower():
                     uczelnia_info = uczelnie_dict.get(osoba["uczelnia"], {})
-                    full_data = {**osoba, **{
-                        "nazwa_uczelni": uczelnia_info.get("nazwa"),
-                        "miasto_uczelni": uczelnia_info.get("miasto"),
-                        "uczelnia_id": uczelnia_info.get("id")
-                    }}
-                    result.append(full_data)
+                    result.append(osoba)
 
     return result[:5]
 
@@ -61,23 +61,14 @@ async def tool_2(request: Request):
         }
         return result
     else:
-        with open("files/uczelnie.json", "r", encoding="utf-8") as f:
-            uczelnie = json.load(f)
-            uczelnie_dict = {u["id"]: u for u in uczelnie}
+        print(f"Request: {request.input}")
         with open("files/badania.json", "r") as f:
             badania = json.load(f)
             # Łączenie danych
             result = []
             for badanie in badania:
-                input_words = request.input.lower().split()
-                # Sprawdź, czy jakiekolwiek słowo pasuje
-                if any(word in badanie.get("nazwa", "").lower() for word in input_words):
-                    uczelnia_info = uczelnie_dict.get(badanie["uczelnia"], {})
-                    full_data = {**badanie, **{
-                        "nazwa_uczelni": uczelnia_info.get("nazwa"),
-                        "miasto_uczelni": uczelnia_info.get("miasto"),
-                        "uczelnia_id": uczelnia_info.get("id")
-                    }}
-                    result.append(full_data)
+                input = request.input.lower()
+                if input in badanie.get("nazwa", "").lower():
+                    result.append(badanie)
 
-    return result[:3]
+    return result[:10]
